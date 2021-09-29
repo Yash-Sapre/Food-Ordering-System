9b32cc6f4fa893ee9669b847f2f5484c918b3e9d
Yash-Sapre <yss052001@gmail.com>
2021-09-29 14:23:55 +0530

    LoginRegisterUi
    UI changes done

--- FoodOrderingWebsite/forms.py
+++ FoodOrderingWebsite/forms.py
@@ -5,9 +5,9 @@ from wtforms.validators import DataRequired,Length, EqualTo
 
 class RegistrationForm(FlaskForm):
     Username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
-    Email = StringField('Email',validators=[DataRequired()])
-    Password = PasswordField('Password',validators=[DataRequired()])
-    ConfirmPassword = PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('Password')])
+    Email = StringField('Email', validators=[DataRequired()])
+    Password = PasswordField('Password', validators=[DataRequired()])
+    ConfirmPassword = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('Password')])
     Submit = SubmitField('Sign Up')
 
 
@@ -15,7 +15,7 @@ class LoginForm(FlaskForm):
     Username = StringField('Username', validators=[DataRequired()])
     Password = PasswordField('Password', validators=[DataRequired()])
     Remember = BooleanField('Remember Field')
-    Submit = SubmitField('Sign Up')
+    Submit = SubmitField('Sign In')
 
 
 
